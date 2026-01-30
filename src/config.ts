dd6766f33bc3b7c3e4a8c1b6ea33e796fcbc6738
kyy008 <keyuyangunis@126.com>
2026-01-30 13:00:41 +0800

    config: 启用umami统计

--- src/config.ts
+++ src/config.ts
@@ -567,10 +567,10 @@ export const widgetConfigs = {
 } as const;
 
 export const umamiConfig = {
-	enabled: false, // 是否显示Umami统计
-	apiKey: import.meta.env.UMAMI_API_KEY || "api_xxxxxxxx", // API密钥优先从环境变量读取，否则使用配置文件中的值
+	enabled: true, // 是否显示Umami统计
+	apiKey: import.meta.env.UMAMI_API_KEY || "api_l8nEg1wyXSgaTvJbhXs4nZnegY6QeusL", // API密钥优先从环境变量读取，否则使用配置文件中的值
 	baseUrl: "https://api.umami.is", // Umami Cloud API地址
 	scripts: `
-<script defer src="XXXX.XXX" data-website-id="ABCD1234"></script>
+<script defer src="https://cloud.umami.is/script.js" data-website-id="be7c8d4e-3ee9-45a9-9f26-347c5001f5e6"></script>
   `.trim(), // 上面填你要插入的Script,不用再去Layout中插入
 } as const;
